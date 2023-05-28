0256b8f7c4b19c98d337517ea57b738925a4a38b
CodeByTanvir01~ <tanvirh484@icloud.com>
2023-05-28 23:22:06 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,13 @@
+//5 test marks of a studnet
+var Mathematics = 75.25;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.50;
+
+//average marks of these subjects
+var totalmark = Mathematics + Biology + Chemistry + Physics + Bangla;
+var averagemark = totalmark/5;
+var averagemark = averagemark.toFixed(2)
+
+console.log(averagemark)
